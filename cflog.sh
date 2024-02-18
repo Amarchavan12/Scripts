b086a8564f4472927a9a29be8fb407365b937687
Amar Chavan <91527897+Amarchavan12@users.noreply.github.com>
2024-02-19 01:11:31 +0530

    Update cflog.sh 19 feb 24

--- cflog.sh
+++ cflog.sh
@@ -1,22 +1,35 @@
-#This script is writtent to fetch 4xx logs from the arious files downloaded from S3 bucket.
-#As an output this script will provide the file name and the 4xx error logs contained in that file.
-
 #!/bin/bash
 dt=$(date "+%d-%m-%y-%H-%M")
 mkdir MOH_$dt
 echo "Created folder named MOH_$dt"
-file=`ls ~/Downloads | grep E1T8KKX7YJPZKB`
+read -p "Enter Distribution name :" dist
+
+file=`ls ~/Downloads | grep $dist`
 
 for i in $file
 do
 	mv ~/Downloads/$i ~/Downloads/MOH_$dt 
 	gzip -d ~/Downloads/MOH_$dt/$i
- 	echo "--------------------------------------------------------------------------------------------------------------"
+        echo "--------------------------------------------------------------------------------------------------------------"
  	echo "File name: $i"
  	echo "--------------------------------------------------------------------------------------------------------------"
 
 #	cat ~/Downloads/MOH_$dt/$i | grep --color -E '404'
 	uz=$(expr " $i" : ' \(.*\)...')
 #	uz=$(echo "$i" | cut -c -37)
- 	grep --color -E '404|403' ~/Downloads/MOH_$dt/$uz
+        #noe=$(awk '$9 ~ /^4[0-9][0-9]$/ { count++ } END { print count }' $uz)
+        #noe=$(grep -o '\s4[0-9]{2}\s' $uz | wc -l)
+        #echo "Totol number of 4xx errors found = $noe"
+        #echo "--------------------------------------------------------------------------------------------------------------"
+        grep --color -E '400|401|402|403|404|405|500|501|502|503|504|505|506|507|508|509|510|511' ~/Downloads/MOH_$dt/$uz
+	#cat $uz | awk '{print $9}' |  grep -E '40[0-5]' | wc -l
+        noe=$(grep --color -E '400|401|402|403|404|405' ~/Downloads/MOH_$dt/$uz | awk '{print $9}' | grep '40[0-5]' | wc -l)
+        noe1=$(grep --color -E '500|501|502|503|504|505|506|507|508|509|510|511' ~/Downloads/MOH_$dt/$uz | awk '{print $9}' | grep '5[0-1][0-9]' | wc -l)
+	#echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	#echo " "
+	echo  "\033[1;31mTotal number of 4XX errors in $uz are $noe\033[0m"
+        echo  "\033[1;31mTotal number of 5XX errors in $uz are $noe1\033[0m"
+	#echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        #echo "--------------------------------------------------------------------------------------------------------------"
+	#echo " "
 done
